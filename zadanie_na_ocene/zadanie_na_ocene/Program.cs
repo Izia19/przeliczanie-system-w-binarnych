cbf3d1408d41f9357751ad9b18148206003926f0
Izia19 <129687432+Izia19@users.noreply.github.com>
2023-12-05 20:41:53 +0100

    Add files via upload

---
+++ zadanie_na_ocene/zadanie_na_ocene/Program.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using zadanie_na_ocene;
+
+namespace zadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Systemy_liczbowe systemy = new Systemy_liczbowe();
+
+            bool petla = true;
+
+            while (petla)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("+-------------------Wybierz działane:-----------------------+");
+                Console.WriteLine("|                                                           |");
+                Console.WriteLine("|   1. Zamien na system binarny/osemkowy/szestnastkowy      |");
+                Console.WriteLine("|   2. Zamiana na systemy (2-16)                            |");
+                Console.WriteLine("|   3. Zamiana z dowolnego na dowolny                       |");
+                Console.WriteLine("|   4. Kalkulator                                           |");
+                Console.WriteLine("|   5. Koniec                                               |");
+                Console.WriteLine("|                                                           |");
+                Console.WriteLine("+-----------------------------------------------------------+");
+                Console.ResetColor();
+
+                int wybor = int.Parse(Console.ReadLine());
+
+                switch (wybor)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Podaj liczbe: ");
+                            int liczba = int.Parse(Console.ReadLine());
+
+                            if(liczba == 0)
+                            {
+                                systemy.komunikaty(2);
+                                break;
+                            }
+
+                            Console.WriteLine($"Liczba dziesiętna {liczba} w: ");
+
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"systemie binarnym: {systemy.system_uniwersalny(liczba, 2)}");
+
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"systemie ósemkowym: {systemy.system_uniwersalny(liczba, 8)}");
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            string liczba_16 = Convert.ToString(liczba, 16);
+                            Console.WriteLine($"systemie szestnastkowy: {liczba_16}");
+
+                            Console.ResetColor();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Podaj liczbe: ");
+                            int liczba = int.Parse(Console.ReadLine());
+
+                            if (liczba == 0)
+                            {
+                                systemy.komunikaty(2);
+                                break;
+                            }
+
+                            Console.WriteLine($"Liczba dziesiętna {liczba} w: ");
+
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"systemie binarnym: {systemy.system_uniwersalny(liczba, 2)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine($"systemie trojkowym: {systemy.system_uniwersalny(liczba, 3)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.WriteLine($"systemie piatkowy: {systemy.system_uniwersalny(liczba, 5)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.WriteLine($"systemie szostkowy: {systemy.system_uniwersalny(liczba, 6)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                            Console.WriteLine($"systemie siodemkowy: {systemy.system_uniwersalny(liczba, 7)}");
+
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"systemie ósemkowym: {systemy.system_uniwersalny(liczba, 8)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                            Console.WriteLine($"systemie dziewiatkowy: {systemy.system_uniwersalny(liczba, 9)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine($"systemie jedenastkowy: {systemy.systemy_od_10(liczba, 11)}");
+
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine($"systemie dwunastkowy: {systemy.systemy_od_10(liczba, 12)}");
+
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine($"systemie trzynastkowy: {systemy.systemy_od_10(liczba, 13)}");
+
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine($"systemie czternastkowy: {systemy.systemy_od_10(liczba, 14)}");
+
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"systemie pietnastkowy: {systemy.systemy_od_10(liczba, 15)}");
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"systemie szestnastkowy: {systemy.systemy_od_10(liczba, 16)}");
+
+                            Console.ResetColor();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Z jakiego systemu chcesz zamienic liczbe: ");
+                            int system_podania = int.Parse(Console.ReadLine());
+
+                            if (system_podania > 16)
+                            {
+                                systemy.komunikaty(1);
+                                break;
+                            }else if (system_podania == 0)
+                            {
+                                systemy.komunikaty(3);
+                                break;
+                            }
+
+                            Console.WriteLine("Podaj tą liczbe: ");
+                            string liczba = Console.ReadLine();
+
+                            if (Convert.ToInt16(liczba) == 0)
+                            {
+                                systemy.komunikaty(2);
+                                break;
+                            }
+
+                            Console.WriteLine("Na jaki system chcesz ja zamienic: ");
+                            int system_zamiany = int.Parse(Console.ReadLine());
+
+                            if (system_zamiany > 16)
+                            {
+                                systemy.komunikaty(1);
+                                break;
+                            }
+                            else if (system_zamiany == 0)
+                            {
+                                systemy.komunikaty(3);
+                                break;
+                            }
+                            else if (system_zamiany < 10)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Liczba: {liczba} w systemie {system_zamiany}: {systemy.system_uniwersalny(systemy.system10(liczba, system_podania), system_zamiany)}");
+                            }
+                            else if(system_zamiany > 10)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Liczba: {liczba} w systemie {system_zamiany}: {systemy.systemy_od_10(systemy.system10(liczba, system_podania), system_zamiany)}");
+                            }
+                            else if(system_zamiany == 10)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Liczba: {liczba} w systemie {system_zamiany}: {systemy.system10(liczba, system_podania)}");
+                            }
+
+                            Console.ResetColor();
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Z jakiego systemu jest pierwsza liczba: ");
+                            int system_podania1 = int.Parse(Console.ReadLine());
+
+                            if (system_podania1 > 16)
+                            {
+                                systemy.komunikaty(1);
+                                break;
+                            }
+                            else if(system_podania1 == 0)
+                            {
+                                systemy.komunikaty(3);
+                                break;
+                            }
+
+                            Console.WriteLine("Podaj tą liczbe: ");
+                            string liczba1 = Console.ReadLine();
+
+                            if (Convert.ToInt16(liczba1) == 0)
+                            {
+                                systemy.komunikaty(2);
+                                break;
+                            }
+
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("+-----Jaką operacje chcesz wykonac:-----+");
+                            Console.WriteLine("|                                       |");
+                            Console.WriteLine("|   1. +                                |");
+                            Console.WriteLine("|   2. -                                |");
+                            Console.WriteLine("|   3. *                                |");
+                            Console.WriteLine("|   4. /                                |");
+                            Console.WriteLine("|                                       |");
+                            Console.WriteLine("+---------------------------------------+");
+                            Console.ResetColor();
+
+                            int wybor_kalkulator = int.Parse(Console.ReadLine());
+                            if ( 1 > wybor_kalkulator || wybor_kalkulator > 4)
+                            {
+                                systemy.komunikaty(4);
+                                break;
+                            }
+
+                            Console.WriteLine("Z jakiego systemu jest druga liczba: ");
+                            int system_podania2 = int.Parse(Console.ReadLine());
+
+                            if (system_podania2 > 16)
+                            {
+                                systemy.komunikaty(1);
+                                break;
+                            }
+                            else if (system_podania2 == 0)
+                            {
+                                systemy.komunikaty(3);
+                                break;
+                            }
+
+                            Console.WriteLine("Podaj tą liczbe: ");
+                            string liczba2 = Console.ReadLine();
+
+                            if (Convert.ToInt16(liczba2) == 0)
+                            {
+                                systemy.komunikaty(2);
+                                break;
+                            }
+
+                            Console.WriteLine("W jakim systemie ma byc wynik: ");
+                            int system_wynik = int.Parse(Console.ReadLine());
+
+                            if (system_wynik > 16)
+                            {
+                                systemy.komunikaty(1);
+                                break;
+                            }
+                            else if (system_wynik == 0)
+                            {
+                                systemy.komunikaty(3);
+                                break;
+                            }
+
+                            int liczba1_zamieniona = 0;
+                            int liczba2_zamieniona = 0;
+
+                            if (system_podania1 == 10)
+                            {
+                                liczba1_zamieniona = Convert.ToInt16(liczba1);
+                            }
+                            if(system_podania2 == 10)
+                            {
+                                liczba2_zamieniona = Convert.ToInt16(liczba2);
+                            }
+                            else
+                            {
+                                liczba1_zamieniona = systemy.system10(liczba1, system_podania1);
+                                liczba2_zamieniona = systemy.system10(liczba2, system_podania2);
+                            }
+
+                            int wynik;
+                            string napis;
+
+                            systemy.kalkulator(liczba1_zamieniona, liczba2_zamieniona, wybor_kalkulator,out napis, out wynik);
+                    
+                            if (system_wynik < 10)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Wynik działania ({napis}) w systemie {system_wynik} to {systemy.system_uniwersalny(wynik,system_wynik)}");
+
+                            }
+                            else if (system_wynik > 10)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Wynik działania ({napis}) w systemie {system_wynik} to {systemy.systemy_od_10(wynik, system_wynik)}");
+                            }
+                            else if (system_wynik == 10)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Wynik działania ({napis}) w systemie {system_wynik} to {wynik}");
+                            }
+
+                            Console.ResetColor();
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            petla = false;
+                            break;
+                        }
+                }
+            }
+
+        }
+    }
+}
---
+++ zadanie_na_ocene/zadanie_na_ocene/Systemy_liczbowe.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_na_ocene
+{
+    public class Systemy_liczbowe
+    {
+        public Systemy_liczbowe() { }
+
+        public string system_uniwersalny(int liczba, int system)
+        {
+            string liczba2 = "";
+            while (liczba > 0)
+            {
+                int reszta = liczba % system;
+                liczba2 = reszta.ToString() + liczba2;
+                liczba = liczba / system;
+            }
+            return liczba2;
+        }
+
+        public string systemy_od_10(int liczba, int system)
+        {
+            string cyfry = "";
+            if(system == 11)
+            {
+                cyfry = "0123456789a"; 
+            }
+            if (system == 12)
+            {
+                cyfry = "0123456789ab";
+            }
+            if (system == 13)
+            {
+                cyfry = "0123456789abc";
+            }
+            if (system == 14)
+            {
+                cyfry = "0123456789abcd";
+            }
+            if (system == 15)
+            {
+                cyfry = "0123456789abcde";
+            }
+            if (system == 16)
+            {
+                cyfry = "0123456789abcdef";
+            }
+
+            string wynik = "";
+
+            while (liczba > 0)
+            {
+                int reszta = liczba % system; 
+                wynik = cyfry[reszta] + wynik; 
+                liczba = liczba / system;
+            }
+
+            return wynik;
+        }
+
+
+        public int system10(string liczba, int system)
+        {
+            int liczba_10 = 0;
+
+            // przechodzimy od lewej do prawej po znakach liczby
+            for (int i = 0; i < liczba.Length; i++)
+            {
+                char cyfra = liczba[i];
+                int przeliczona_cyfra = 0;
+
+                if (cyfra >= '0' && cyfra <= '9') //sprawdza czy znak cyfra jest cyfra od 0 - 9
+                {
+                    przeliczona_cyfra = int.Parse(cyfra.ToString()); 
+                }
+                else if (cyfra >= 'A' && cyfra <= 'Z') //sprawdza czy znak cyfra jest litera od A - Z
+                {
+                    przeliczona_cyfra = cyfra - 'A' + 10;
+                }
+                else if(cyfra >= 'a' && cyfra <= 'z')  //sprawdza czy znak cyfra jest litera od a - z
+                {
+                    przeliczona_cyfra = cyfra - 'a' + 10;
+                }
+                else
+                {
+                    throw new ArgumentException("Nieprawidłowa liczba.");
+                }
+
+                liczba_10 = liczba_10 * system + przeliczona_cyfra;
+            }
+
+            return liczba_10;
+
+        }
+
+        public void kalkulator(int liczba1, int liczba2, int ktore_dzialanie, out string napis, out int wynik)
+        {
+             wynik = 0;
+             napis = "";
+
+            if (ktore_dzialanie == 1)
+            {
+                wynik = liczba1 + liczba2;
+                napis = $"{liczba1} + {liczba2}";
+            }
+            else if (ktore_dzialanie == 2)
+            {
+                wynik = liczba1 - liczba2;
+                napis = $"{liczba1} - {liczba2}";
+            }
+            else if (ktore_dzialanie == 3)
+            {
+                wynik = liczba1 * liczba2;
+                napis = $"{liczba1} * {liczba2}";
+            }
+            else if (ktore_dzialanie == 4)
+            {
+                wynik = liczba1 / liczba2;
+                napis = $"{liczba1} / {liczba2}";
+            }
+        } 
+
+        public void komunikaty(int ktory)
+        {
+            if(ktory == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("+-----------------------------------------------------------------------------+");
+                Console.WriteLine("|                                                                             |");
+                Console.WriteLine("|   Przepraszamy program nie obsluguje wiekszych systemów liczbowych niż 16   |");
+                Console.WriteLine("|                                                                             |");
+                Console.WriteLine("+-----------------------------------------------------------------------------+");
+                Console.ResetColor();
+            }
+            else if(ktory == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("+---------------------------+");
+                Console.WriteLine("|                           |");
+                Console.WriteLine("|  !Podaj poprawna liczbe!  |");
+                Console.WriteLine("|                           |");
+                Console.WriteLine("+---------------------------+");
+                Console.ResetColor();
+            }
+            else if(ktory == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("+------------------------------------------+");
+                Console.WriteLine("|                                          |");
+                Console.WriteLine("|   !System liczby nie moze byc rowny 0!   |");
+                Console.WriteLine("|                                          |");
+                Console.WriteLine("+------------------------------------------+");
+                Console.ResetColor();
+            }
+            else if (ktory == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("+------------------------------+");
+                Console.WriteLine("|                              |");
+                Console.WriteLine("|   Nie ma takiego dzialania   |");
+                Console.WriteLine("|                              |");
+                Console.WriteLine("+------------------------------+");
+                Console.ResetColor();
+            }
+        }
+    }
+}
